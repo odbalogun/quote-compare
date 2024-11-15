6248a737a1dd4176a0dc10535f15990cf92e56b0
Oduntan Balogun <153731817+oduntan-balogun@users.noreply.github.com>
2024-11-15 17:01:37 +0100

    Added a url to list all users

--- api/serializers.py
+++ api/serializers.py
@@ -2,11 +2,22 @@ from core.models import User
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        style={'input_type': 'password', 'placeholder': 'Password'}
+    )
+
     class Meta:
         model = User
         fields = ["id", "email", "password"]
-        extra_kwargs = {"password": {"write_only": True}}
+        # extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
+class UserAllFieldsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from core.models import User
 from rest_framework import generics
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserAllFieldsSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+class FetchAllUsersView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserAllFieldsSerializer
     permission_classes = [AllowAny]
